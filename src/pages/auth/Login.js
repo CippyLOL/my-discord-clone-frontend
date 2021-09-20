ba5c5d1b371e87260ced5aa72181779087632640
Gillianne Papasin <47998428+CippyLOL@users.noreply.github.com>
2021-09-20 12:22:14 +0800

    Form Validation
    Added Formik and Yup

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -4,6 +4,19 @@ import { ReactComponent as Image } from "assets/svg/authBanner.svg";
 import { Link } from "react-router-dom";
 import Head from "components/Head";
 
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid Email").required("Email Required"),
+  username: Yup.string().required("Username Required"),
+  password: Yup.string()
+    .min(6, "Password needs more than 6 characters")
+    .max(20, "Password needs less than 20 characters")
+    .required("Password Required"),
+  passwordConfirmation: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match')
+});
+
 function Login() {
   return (
     <>
@@ -24,44 +37,68 @@ function Login() {
             </button>
           </div>
           <p className="my-2 mt-4 text-center text-gray-400">-- OR --</p>
-          <form className="space-y-4">
-            <div className="space-y-2">
-              <input
-                type="text"
-                placeholder="Email"
-                className="w-full px-2 py-4 transition delay-75 transform bg-gray-800 rounded-md focus:outline-black focus:scale-105"
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                className="w-full px-2 py-4 transition delay-75 transform bg-gray-800 rounded-md focus:outline-black focus:scale-105"
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                className="w-full px-2 py-4 transition delay-75 transform bg-gray-800 rounded-md focus:outline-black focus:scale-105"
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                className="w-full px-2 py-4 transition delay-75 transform bg-gray-800 rounded-md focus:outline-black focus:scale-105"
-              />
-            </div>
-            <div className="flex justify-between">
-              <a
-                href="/"
-                className="inline-block text-gray-400 hover:underline"
+          <Formik
+            initialValues={{ username: "", email: "", password: "" }}
+            validationSchema={validationSchema}
+            //  onSubmit outputs values for now
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            <Form className="space-y-4">
+              <div className="space-y-2">
+                <Field
+                  type="text"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                  className="w-full px-2 py-4 transition delay-75 transform bg-gray-800 rounded-md focus:outline-black focus:scale-105"
+                />
+                <ErrorMessage name="email" />
+                <Field
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="Username"
+                  className="w-full px-2 py-4 transition delay-75 transform bg-gray-800 rounded-md focus:outline-black focus:scale-105"
+                />
+                <ErrorMessage name="username" />
+                <Field
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                  className="w-full px-2 py-4 transition delay-75 transform bg-gray-800 rounded-md focus:outline-black focus:scale-105"
+                />
+                <ErrorMessage name="password" />
+                <Field
+                  type="password"
+                  name="passwordConfirmation"
+                  id="password"
+                  placeholder="Confirm Password"
+                  className="w-full px-2 py-4 transition delay-75 transform bg-gray-800 rounded-md focus:outline-black focus:scale-105"
+                />
+                <ErrorMessage name="passwordConfirmation" />
+              </div>
+              <div className="flex justify-between">
+                <a
+                  href="/"
+                  className="inline-block text-gray-400 hover:underline"
+                >
+                  Forgot Password?
+                </a>
+              </div>
+              <button
+                name="login"
+                className="w-full p-2 text-lg rounded-md gradient"
               >
-                Forgot Password?
-              </a>
-            </div>
-            <button
-              name="login"
-              className="w-full p-2 text-lg rounded-md gradient"
-            >
-              Login
-            </button>
-          </form>
+                Login
+              </button>
+            </Form>
+          </Formik>
           <p className="my-4 text-center text-gray-400">
             Not a member yet ?{" "}
             <Link to="/register" className="text-white underline">
